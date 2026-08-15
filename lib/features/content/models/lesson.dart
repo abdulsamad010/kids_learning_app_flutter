2801e978df5a67c32f0a0aff40c449d88ab83ba5
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-16 00:33:06 +0500

    Add lesson model

---
+++ lib/features/content/models/lesson.dart
@@ -0,0 +1,11 @@
+class Lesson {
+  final int lessonId;
+  final int subjectId;
+  final String title;
+
+  Lesson({
+    required this.lessonId,
+    required this.subjectId,
+    required this.title,
+  });
+}
