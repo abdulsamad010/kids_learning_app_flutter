b77d237ce2e13034f3ebed3c43a6f77709b5b11c
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-16 00:33:18 +0500

    Add quiz model

---
+++ lib/features/quiz/models/quiz.dart
@@ -0,0 +1,13 @@
+class Quiz {
+  final int quizId;
+  final int lessonId;
+  final int subjectId;
+  final String title;
+
+  Quiz({
+    required this.quizId,
+    required this.lessonId,
+    required this.subjectId,
+    required this.title,
+  });
+}
