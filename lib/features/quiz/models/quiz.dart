0751cffc8d421cbd890df09882f56ae8afeb2ff4
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-16 01:51:16 +0500

    Update quiz model

--- lib/features/quiz/models/quiz.dart
+++ lib/features/quiz/models/quiz.dart
@@ -3,11 +3,13 @@ class Quiz {
   final int lessonId;
   final int subjectId;
   final String title;
+  final String type;
 
   Quiz({
     required this.quizId,
     required this.lessonId,
     required this.subjectId,
     required this.title,
+    required this.type,
   });
 }
