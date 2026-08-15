36bb97752d9826e3c35fd568b90043bbeae61de8
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-16 01:51:39 +0500

    Implement multiple choice widget

---
+++ lib/features/quiz/widgets/multiple_choice.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../models/question.dart';
+import '../models/answer.dart';
+
+class MultipleChoice extends StatelessWidget {
+  final Question question;
+  final List<Answer> answers;
+
+  const MultipleChoice({
+    super.key,
+    required this.question,
+    required this.answers,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(screenWidth * 0.04),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(question.questionText),
+            SizedBox(height: screenWidth * 0.04),
+
+            for (Answer answer in answers)
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: screenWidth * 0.02,
+                ),
+                child: Text(answer.answerText),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
